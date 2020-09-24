79f2a899772e79e1dd8131ed07c56050e0fadbd3
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-09-24 22:20:48 +0300

    bug fix

--- app/src/main/java/ru/mvlikhachev/stopsmoke/Activities/AboutActivity.kt
+++ app/src/main/java/ru/mvlikhachev/stopsmoke/Activities/AboutActivity.kt
@@ -1,12 +1,39 @@
 package ru.mvlikhachev.stopsmoke.Activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_about.*
 import ru.mvlikhachev.stopsmoke.R
+
 
 class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        supportActionBar!!.hide()
+
+        vkImageView.setOnClickListener { view ->
+            val browserIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://vk.com/mv.likhachev"))
+            startActivity(browserIntent)
+        }
+
+        instagramImageView.setOnClickListener { view ->
+            val browserIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/mv.likhachev"))
+            startActivity(browserIntent)
+        }
+        telegramImageView.setOnClickListener { view ->
+            val browserIntent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/mlikhachev"))
+            startActivity(browserIntent)
+        }
+
     }
+
+
 }
